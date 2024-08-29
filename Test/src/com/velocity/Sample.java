44593e47d4bd4e243ee2eb906c9654738eebf05a
Mohan Mahadik <mohanmahadik77@gmail.com>
2024-08-29 21:15:39 +0530

    add code to featuer branch

--- Test/src/com/velocity/Sample.java
+++ Test/src/com/velocity/Sample.java
@@ -5,8 +5,9 @@ public class Sample {
 	public static void main(String[] args) {
 		for(int i = 1; i<=10;i++) {
 			System.out.println(i);
+			
 		}
-
+       System.out.println("add code to feature branch");
 	}
 
 }
